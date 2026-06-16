e29db78e0627b33516ab4928d60afa49f492751b
Mateusz-Bak7 <mateuszbak2010@gmail.com>
2026-06-16 20:50:38 +0200

    Wreszcie udalo mi sie naprawic vectory

--- practice2.cpp
+++ practice2.cpp
@@ -44,30 +44,44 @@ public:
 void drawboard(Bird bird, vector<Pipe> pipes){
 		for(int y = 0; y < 15; y++){
 			for(int x = 0; x < 70; x++){
+				bool drawn = false;
 				if(bird.x == x && bird.y == y){
 					cout << "O";
+					drawn = true;
 				}
-				else if(bird.x != x || bird.y != y){
-					cout << ".";
-				}
-				else{
+				if(drawn == false){
 					for(int i = 0; i < pipes.size(); i++){
-						for(int i = 0; i < pipes.size(); i++){
-							if(y <= pipes[i].gapy && pipes[i].gapy + pipes[i].gapsize > y){
+						if(pipes[i].x == x){ // jesli jest tu rura, sprawdz czy dziura itp.
+							if (y >= pipes[i].gapy && pipes[i].gapy + pipes[i].gapsize > y){
 								cout << ".";
 							}
 							else{
 								cout << "|";
 							}
-						}
+							drawn = true;
+					}
 					}
 				}
-			}
-			cout << endl;
+				if(drawn == false){
+					cout << ".";
+				}
+				}
+				cout << endl;
 			}
 		}
 
-bool is_running = false;
+void gameover(){
+	cout << endl;
+		cout << R"(  ######   #####  ###    ### #######      ######  ##    ## ####### ######  
+ ##       ##   ## ####  #### ##          ##    ## ##    ## ##      ##   ## 
+ ##   ### ####### ## #### ## #####       ##    ## ##    ## #####   ######  
+ ##    ## ##   ## ##  ##  ## ##          ##    ##  ##  ##  ##      ##   ## 
+  ######  ##   ## ##      ## #######      ######    ####   ####### ##   ## 
+)";
+
+
+}
+bool is_running = true;
 int main(){
 	vector<Pipe> pipes;
 	int randgap;
@@ -75,23 +89,19 @@ int main(){
 	int framecounter = 0;
 	string userchoice;
 	int counter = 0;
+	bool hit;
 	Bird bird(5, 3);
 	pipes.push_back(Pipe(69, 4, 3));
 	pipes.push_back(Pipe(53, 6, 2));
 	pipes.push_back(Pipe(39, 5, 4));
 
-	display();
-	cin >> userchoice;
-	if(userchoice == "3"){
-		is_running = true;
-	}
 
-	while(is_running){
+	while(is_running){ // uzycie wszystkich instrukcji do gry, glowna petla gry
 	system("cls");
 
 	srand(time(NULL));
 	randgap = rand() % 15;
-	randsize = rand() % 5;
+	randsize = rand() % 5 + 1;
 	
 	drawboard(bird, pipes);
 	if(_kbhit()){
@@ -99,6 +109,7 @@ int main(){
 		key = _getch();
 		if(key == ' '){
 			bird.jump();
+			Sleep(40);
 		}
 	}
 	for(int i = 0; i < pipes.size(); i++){
@@ -110,40 +121,31 @@ int main(){
 	}
 	if(bird.y > 15 || bird.y <= 0){
 		
-		cout << endl;
-		cout << R"(  ######   #####  ###    ### #######      ######  ##    ## ####### ######  
- ##       ##   ## ####  #### ##          ##    ## ##    ## ##      ##   ## 
- ##   ### ####### ## #### ## #####       ##    ## ##    ## #####   ######  
- ##    ## ##   ## ##  ##  ## ##          ##    ##  ##  ##  ##      ##   ## 
-  ######  ##   ## ##      ## #######      ######    ####   ####### ##   ## 
-)";
+		gameover();
 
 		is_running = false;
 	}
 	for(int i = 0; i < pipes.size(); i++){
-	if(bird.x == pipes[i].x && bird.x){ // Warunek do hitboxa
-		if(pipes[i].gapy <= bird.y && pipes[i].gapy + pipes[i].gapsize > bird.y){
-			break;
-		}
-		else{
-			cout << endl;
-		cout << R"(  ######   #####  ###    ### #######      ######  ##    ## ####### ######  
- ##       ##   ## ####  #### ##          ##    ## ##    ## ##      ##   ## 
- ##   ### ####### ## #### ## #####       ##    ## ##    ## #####   ######  
- ##    ## ##   ## ##  ##  ## ##          ##    ##  ##  ##  ##      ##   ## 
-  ######  ##   ## ##      ## #######      ######    ####   ####### ##   ## 
-)";
-
-			is_running == false;
-			break;
-		}
+		if(hit == false){
+			if(bird.x == pipes[i].x){ // Warunek do hitboxa
+				if(bird.y >= pipes[i].gapy && pipes[i].gapy + pipes[i].gapsize > bird.y){
+					hit = false;
+				}
+				else{
+					hit = true;
+				}
+			}
 	}
+		else{
+			gameover();
+			is_running = false;
+		}
 	}
 	framecounter++;
 
-	for(int i = 0; i < pipes.size(); i++){
+	for(int i = 0; i < pipes.size(); i++){ // warunek do przesuniecia rury spowrotem w prawo
 		if(pipes[i].x < 0){
-			pipes[i].x = 49;
+			pipes[i].x = 69;
 			pipes[i].gapy = randgap;
 			pipes[i].gapsize = randsize;
 		}
